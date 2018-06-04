674e6eac291e4d1436700f02373fbcd34605b917
Natali Hreshchyshyn <natalihreshchyshyn@gmail.com>
2018-06-04 12:03:23 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,28 @@
+let n;
+do {
+    alert('Please enter a natural number');
+    n = prompt('Enter n');
+} while (n <= 0); 
+
+// function fib(n) {
+//     if (n === 1 || n === 2) {
+//         return 1;
+//     }
+//     else {
+//         return n = fib(n - 1) + fib(n - 2);
+//     }
+// }
+// alert(fib(n));
+
+let a = 1;
+let b = 1;
+
+function fib(n) {
+    for (let i = 3; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+alert(fib(n));
